1b3cf772a96f8526ec177a3a51043b2c9f1baf7a
Reiyo OISHI <reiyo.icepp@gmail.com>
2023-12-07 12:34:20 +0900

    Updated payload parsor

--- src/payloadParsor.py
+++ src/payloadParsor.py
@@ -4,9 +4,11 @@ def PayloadParsor(payload: dict, ignore_image: bool=True):
   '''
   OpenAI Python Library内で設定するpayloadを解析する。
   '''
+  print(payload)
+
   _payload = copy.deepcopy(payload)
-  for imessage in len(_payload['messages']):
-    for icontent in len(_payload['messages'][imessage]['content']):
+  for imessage in range(len(_payload['messages'])):
+    for icontent in range(len(_payload['messages'][imessage]['content'])):
       if _payload['messages'][imessage]['content'][icontent]['type'] == 'image_url' and ignore_image:
         _payload['messages'][imessage]['content'][icontent]['image_url']['url'] = 'Image information'
 
